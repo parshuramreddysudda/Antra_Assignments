b2ce4c5bdd95f99b818e249a2e9c19704c5b9e0a
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-04-22 21:50:49 -0400

    Assignment Added

--- Rider Notes/Solution1/Assignment3/obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
+++ Rider Notes/Solution1/Assignment3/obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a834888a03870b49f4e85a03d38262d71acbe74d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f64b759f8603653624b36b87e3bc4256e8dec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
