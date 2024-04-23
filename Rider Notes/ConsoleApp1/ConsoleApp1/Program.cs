06f64b759f8603653624b36b87e3bc4256e8dec1
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-04-22 20:30:53 -0400

    Create Program.cs

---
+++ Rider Notes/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+class PermanentEmployee
+{
+    public List<string> GetAllEmployes()
+    {
+        return new List<string>();
+    }
+}
+
+class NewClass
+{
+    public static void Main(string[] args)
+    {
+        PermanentEmployee pEmployee = new PermanentEmployee();
+        pEmployee.GetAllEmployes();
+        
+    }
+    
+}
