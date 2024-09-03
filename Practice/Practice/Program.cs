143fab3207886ed1d7066fba4c1f5d3f34a622b7
Parshuram Reddy <parshuram.sudda@gmail.com>
2024-09-03 18:36:33 -0400

    Updated the code

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+
+class MyClass
+{
+    
+    
+    public static void Main(string[] args)
+    {
+        SqlConnection con = null;
+
+        try
+        {
+
+            con = new SqlConnection("data source=. database=Ecommerce; integrated security==SSPI");
+            SqlCommand cm = new SqlCommand("select * from products");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
